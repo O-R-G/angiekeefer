cb6685a938cc2396b9bd2e0e09be3cd40645fd99
aaak-kaaa <angie@angiekeefer.com>
2025-09-20 13:05:46 -0400

    Update config.php

--- static/php/config.php
+++ static/php/config.php
@@ -3,13 +3,13 @@
     site-specific config
 */
 
-$site_title = 'nope';
-$head = 'Second';
-$site = 'thoughts';
+$site_title = 'w-w-w';
+$head = 'Hello';
+$site = 'w-w-w-orld';
 $home = $head . ", " . $site;
 $card_default = '/media/jpg/card-default.jpg';
 $logo_src = '/media/jpg/logo.jpg';
-$description = 'Second thoughts';
+$description = 'Hello, w-w-w-orld';
 $site_url = isset($_SERVER['HTTPS']) && $_SERVER['HTTPS'] != 'off' ? 'https' : 'http';
 $site_url .= '://' . $_SERVER['SERVER_NAME'];
 $og_locale = 'en_US';
