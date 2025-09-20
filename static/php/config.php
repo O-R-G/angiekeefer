0872464b7374c89e7509498149da649c97f60698
aaak-kaaa <angie@angiekeefer.com>
2025-09-20 12:24:15 -0400

    Update config.php

--- static/php/config.php
+++ static/php/config.php
@@ -5,11 +5,11 @@
 
 $site_title = 'w-w-w';
 $head = 'Hello';
-$site = 'you.';
+$site = 'w-w-w-orld.';
 $home = $head . ", " . $site;
 $card_default = '/media/jpg/card-default.jpg';
 $logo_src = '/media/jpg/logo.jpg';
-$description = 'Hello, you.';
+$description = 'Hello, w-w-w-orld.';
 $site_url = isset($_SERVER['HTTPS']) && $_SERVER['HTTPS'] != 'off' ? 'https' : 'http';
 $site_url .= '://' . $_SERVER['SERVER_NAME'];
 $og_locale = 'en_US';
